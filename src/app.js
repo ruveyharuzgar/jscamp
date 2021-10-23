1742d7a710caf2657fcd4dc3cfd55de1d2a64f0c
ruveyharuzgar <ruveyha.ruzgar@rempeople.com>
2021-10-24 01:11:39 +0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,9 @@
+console.log("merhaba kodlama.io")
+
+let konutKredileri=["Konut Kredisi", "Emlak Konut Kredisi","Kamu Konut Kredisi"]
+
+console.log("<ul>")
+for (let i =0 ;i<3; i++) {
+    console.log("<li>"+ konutKredileri[i]+ "</li>")
+}
+console.log("</ul>")
